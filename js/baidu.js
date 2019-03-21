4fdf6b254c73ffe2b6b1af154bcbb929ca74a2a7
foxzzz <i@foxzzz.com>
2019-03-21 11:32:27 +0800

    update

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?52fe443066b2872e582afd789481c10f";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
