1a13aed018dff4dc13b63ed14cb38c47562c04f1
foxzzz <i@foxzzz.com>
2021-05-20 11:22:40 +0800

    update

--- js/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?52fe443066b2872e582afd789481c10f";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
